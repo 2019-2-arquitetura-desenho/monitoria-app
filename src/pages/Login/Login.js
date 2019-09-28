a240e6ebf934424a21b1a9b875632f589a584fa6
lucas <lucasmacielaguiar@gmail.com>
2019-09-27 21:28:47 -0300

    #4 draft of Page Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,32 +2,54 @@ import React from 'react';
 
 import './Login.css';
 
+import logo from '../../assets/logo_full.png';
 import classroomImg from '../../assets/classroom.svg';
 
-function Login() {
+const Login = () => {
     return (
-        <div className="Container">
+        <div className="Container" >
             <div className="Content">
                 <div className="Panel-left">
                     <div className="Title">
                         <h1 id="Title-part1">Monitoria</h1>
                         <h1 id="Title-part2">FGA</h1>
                     </div>
-                    <div id="DescApp">
+                    <div className="DescApp">
                         <text>Descrição detalhada do app e
                             apresentação de funcionalidades, é
                             nesse texto
                         </text>
                     </div>
-                    <img className="Classroom-img"
-                        src={classroomImg}
-                    />
+                    <div className="Content-img">
+                        <img className="Classroom-img"
+                            src={classroomImg}
+                        />
+                    </div>
                 </div>
-                <div className="Panel-login">
 
+                <div className="Panel-login">
+                    <div className="Content-logo">
+                        <img id="Logo" src={logo} />
+                    </div>
+
+                    <div className="Content-userLogin">
+                        <input className="UserLogin-input" type="text" value="E-mail" />
+                        <input className="UserLogin-input" type="text" value="Senha" />
+                    </div>
+
+                    <div className="Content-userButtons">
+                        <button className="UserButton">Entrar</button>
+                        <button className="UserButton">Criar Conta</button>
+                    </div>
+
+                    <div className="Content-professorButton">
+                        <button className="ProfessorButton">
+                            Entrar Como Professor
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
 
