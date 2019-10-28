bb718fd8c0d730df51083b3385e9c3e4a2b86134
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-10-28 09:44:28 -0300

    Implement root skin

---
+++ src/skins/root.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Root = ({ store }) => (
+    <Provider store={ store }>
+        <Router>
+            <Route></Route>
+        </Router>
+    </Provider>
+)
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
+}
+
+export default Root
