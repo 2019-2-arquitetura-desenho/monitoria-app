8ee4e89d9d886d5557e163ee4bcd7b2d5c95858b
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-10-29 18:20:24 -0300

    Implement InputText

--- src/containers/register.js
+++ src/containers/register.js
@@ -4,24 +4,100 @@ import {
     createMuiTheme,
     MuiThemeProvider,
     CssBaseline, 
-    Typography,
     Grid,
-    Box
+    Typography
 } from '@material-ui/core';
+
+import MainTitle from './components/MainTitle/mainTitle';
+import InputText from './components/InputText/inputText';
 
 
 class Register extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+
+            password: '',
+            showPassword: false,
+            confirmPassword: '',
+            showConfirmPassword: false
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
+        this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this);
+        this.handleClickShowConfirmPassword = this.handleClickShowConfirmPassword.bind(this);
+        this.handleMouseDownConfirmPassword = this.handleMouseDownConfirmPassword.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.id]: event.target.value });
+    }
+
+    handleClickShowPassword() {
+        this.setState({ showPassword: !this.state.showPassword });
+    }
+
+    handleMouseDownPassword(event) {
+        event.preventDefault();
+    }
+
+    handleClickShowConfirmPassword() {
+        this.setState({ showConfirmPassword: !this.state.showConfirmPassword });
+    }
+
+    handleMouseDownConfirmPassword(event) {
+        event.preventDefault();
+    }
+
     render() {
+        const { name, email, password, showPassword, confirmPassword, showConfirmPassword} = this.state;
+
         return (
             <MuiThemeProvider theme={ theme }>
                 <CssBaseline />
                 <Grid container style={ styles.screenBackground }>
                     <Typography component="div" style={ styles.screenContent }>
                         <Grid container spacing={1}>
-                            <Grid item xs={12}>
-                                <Typography variant="h2" style={ styles.mainTitle }>
-                                    Criar Conta
-                                </Typography>
+                            <MainTitle title="Criar Conta"/>
+                            <Grid item xs={12} sm={6}>
+                                <InputText
+                                    id="name"
+                                    type="text"
+                                    label="Nome*"
+                                    value={ name }
+                                    onChange={ this.handleChange }
+                                />
+                                <InputText
+                                    id="email"
+                                    type="email"
+                                    label="Email*"
+                                    value={ email }
+                                    onChange={ this.handleChange }
+                                />
+                                <InputText
+                                    id="password"
+                                    type={ showPassword ? "text" : "password" }
+                                    label="Senha*"
+                                    value={ password }
+                                    onChange={ this.handleChange }
+                                    onClickShow={ this.handleClickShowPassword }
+                                    onMouseDown={ this.handleMouseDownPassword }
+                                    valueVisibility={ showPassword }
+                                />
+                                <InputText
+                                    id="confirmPassword"
+                                    type={ showConfirmPassword ? "text" : "password" }
+                                    label="Confirmar Senha*"
+                                    value={ confirmPassword }
+                                    onChange={ this.handleChange }
+                                    onClickShow={ this.handleClickShowConfirmPassword }
+                                    onMouseDown={ this.handleMouseDownConfirmPassword }
+                                    valueVisibility={ showConfirmPassword }
+                                />
                             </Grid>
                         </Grid>
                     </Typography>
@@ -49,16 +125,7 @@ const styles = {
     screenContent: {
         height: "85%",
         width: "92%",
-        display: "flex", 
         backgroundColor: "#ffffff"
-    },
-    mainTitle: {
-        marginTop: "3%",
-        marginBottom: "3%",
-        textAlign: "center",
-        color: "#267cc1",
-        fontWeight: "bold",
-        // fontFamily: "fontFamily"
     }
 }
 
