0c5b162b4f734f17a0a0479b0a5d74d1184794d6
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-04 09:15:37 -0300

    Refatoração

--- src/skins/privateRoute.js
+++ src/skins/privateRoute.js
@@ -3,7 +3,9 @@ import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 
-const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
+const PrivateRoute = ({ 
+    component: Component, isAuthenticated, ...rest 
+}) => (
     <Route { ...rest } render={ props => (
         isAuthenticated ? (
             <Component { ...props }/>
