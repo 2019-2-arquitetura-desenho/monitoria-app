76978853e8f531f2a8473cb5e82fbfed19ff6f75
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-05 15:34:25 -0300

    Implement requisition

--- src/containers/register.js
+++ src/containers/register.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { register, restartRegister } from '../store/actions';
 
-import { 
+import {
     createMuiTheme,
     MuiThemeProvider,
     CssBaseline, 
@@ -12,6 +14,7 @@ import MainTitle from './components/MainTitle/mainTitle';
 import InputText from './components/InputText/inputText';
 import FileSubmit from './components/FileSubmit/fileSubmit';
 import SubmitButton from './components/SubmitButton/submitButton';
+import MainError from './components/MainError/mainError';
 
 
 class Register extends React.Component {
@@ -33,8 +36,9 @@ class Register extends React.Component {
             isFileSubmit: false,
             fileSubmit: [],
 
+            mainError: "",
             inputErrors: {},
-        }
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
@@ -48,6 +52,25 @@ class Register extends React.Component {
         this.handleConfirmUploadDialog = this.handleConfirmUploadDialog.bind(this);
         this.onUpdateFileUploadScreen = this.onUpdateFileUploadScreen.bind(this);
         this.onPressSubmit = this.onPressSubmit.bind(this);
+    }
+
+    componentDidUpdate() {
+        if (this.props.requisitionError !== undefined && this.state.loading===true){
+
+            if (this.props.requisitionError === "Error: Network Error"){
+                this.props.restartRegister();
+
+                console.log("Erro de Rede");
+                this.setState({
+                    mainError: "Erro! Verifique sua conexão com a internet e tente novamente mais tarde.",
+                    loading: false
+                });
+            }
+            // else {
+            //     console.log(this.props.requisitionError.data.email);
+            //     this.setState({loading: false});     
+            // }
+        }
     }
 
     handleChange(event) {
@@ -79,18 +102,18 @@ class Register extends React.Component {
     }
 
     onUpdateFileDialog(fileItems) {
-        this.setState({ fileSubmit: fileItems.map(fileItem => fileItem.file) })
+        this.setState({ fileSubmit: fileItems.map(fileItem => fileItem.file) });
     }
 
     handleCancelUploadDialog() {
-        this.setState({ isUploadDialogOpen: false })
+        this.setState({ isUploadDialogOpen: false });
     }
 
     handleConfirmUploadDialog() {
         if (this.state.fileSubmit === undefined){
             this.setState({ isUploadDialogOpen: false });
         } else if (this.state.fileSubmit === 0){
-            this.setState({ isUploadDialogOpen: false })
+            this.setState({ isUploadDialogOpen: false });
         } else {
             this.setState({ isUploadDialogOpen: false, isFileSubmit: true });
         }
@@ -98,22 +121,22 @@ class Register extends React.Component {
 
     onUpdateFileUploadScreen(fileItems) {
         if (this.state.fileSubmit.length !== fileItems.map(fileItem => fileItem.file).length){
-            this.setState({ fileSubmit: fileItems.map(fileItem => fileItem.file) })
+            this.setState({ fileSubmit: fileItems.map(fileItem => fileItem.file) });
         }
     }
 
     onPressSubmit() {
-        var inputErrors = {}
+        var inputErrors = {};
 
-        let f_errorName = false
-        let f_errorEmail = false
-        let f_errorPassword = false
-        let f_errorConfirmPassword = false
-        
-        let name = this.state.name
-        let email = this.state.email
-        let password = this.state.password
-        let confirmPassword = this.state.confirmPassword
+        let f_errorName = false;
+        let f_errorEmail = false;
+        let f_errorPassword = false;
+        let f_errorConfirmPassword = false;
+
+        let name = this.state.name;
+        let email = this.state.email;
+        let password = this.state.password;
+        let confirmPassword = this.state.confirmPassword;
 
         if(!name){
             inputErrors['name'] = "Digite o nome";
@@ -127,7 +150,7 @@ class Register extends React.Component {
             inputErrors['email'] = "Digite um e-mail válido";
             f_errorEmail = true;
           }
-      
+
           if (!password){
             inputErrors['password'] = "Digite uma senha";
             f_errorPassword = true;
@@ -151,84 +174,95 @@ class Register extends React.Component {
             }
           }
 
-          this.setState({ inputErrors: inputErrors })
-   
+          this.setState({ inputErrors: inputErrors });
+
         if (!f_errorName && !f_errorEmail && !f_errorPassword && !f_errorConfirmPassword){
-            // enviar requisição de registro
-      
-            this.setState({ loading: true });
-          }
+            this.setState({ loading: true, mainError: "" });
+            this.props.register(name, email, password, this.state.fileSubmit);
+        }
     }
 
+    registerForm() {
+        const { 
+            mainError, name, email, password, showPassword, confirmPassword, showConfirmPassword,
+            fileSubmit, isFileSubmit, isUploadDialogOpen, inputErrors, loading
+        } = this.state;
+    
+        if(loading){
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <Grid item xs={12} sm={6}>
+                    <MainError 
+                        error={mainError}   
+                    />
+                    <InputText
+                        id="name"
+                        type="text"
+                        label="Nome*"
+                        value={ name }
+                        onChange={ this.handleChange }
+                        error={ inputErrors.name }
+                    />
+                    <InputText
+                        id="email"
+                        type="email"
+                        label="Email*"
+                        value={ email }
+                        onChange={ this.handleChange }
+                        error={ inputErrors.email }
+                    />
+                    <InputText
+                        id="password"
+                        type={ showPassword ? "text" : "password" }
+                        label="Senha*"
+                        value={ password }
+                        onChange={ this.handleChange }
+                        onClickShow={ this.handleClickShowPassword }
+                        onMouseDown={ this.handleMouseDownPassword }
+                        valueVisibility={ showPassword }
+                        error={ inputErrors.password }
+                    />
+                    <InputText
+                        id="confirmPassword"
+                        type={ showConfirmPassword ? "text" : "password" }
+                        label="Confirmar Senha*"
+                        value={ confirmPassword }
+                        onChange={ this.handleChange }
+                        onClickShow={ this.handleClickShowConfirmPassword }
+                        onMouseDown={ this.handleMouseDownConfirmPassword }
+                        valueVisibility={ showConfirmPassword }
+                        error={ inputErrors.confirmPassword }
+                    />
+                    <FileSubmit 
+                        isFileSubmit={ isFileSubmit }
+                        fileSubmit={ fileSubmit }
+                        onButtonUploadClick={ this.handleClickOpenUploadDialog }
+                        isUploadDialogOpen={ isUploadDialogOpen }
+                        onCloseUploadDialog={ this.handleCloseUploadDialog }
+                        onUpdateFileUploadDialog={ this.onUpdateFileUploadDialog }
+                        onClickCancelUploadDialog={ this.handleCancelUploadDialog }
+                        onClickConfirmUploadDialog={ this.handleConfirmUploadDialog }
+                        onUpdateFileUploadScreen={ this.onUpdateFileUploadScreen }    
+                    />
+                    <SubmitButton 
+                        titleButton="Criar Conta"
+                        onClickSubmitButton={ this.onPressSubmit }
+                    />
+                </Grid>
+            )
+        }
+    }
 
     render() {
-        const { 
-            name, email, password, showPassword, confirmPassword, showConfirmPassword,
-            fileSubmit, isFileSubmit, isUploadDialogOpen, inputErrors
-        } = this.state;
-
         return (
             <MuiThemeProvider theme={ theme }>
                 <CssBaseline />
                 <Grid container style={ styles.screenBackground }>
                     <Typography component="div" style={ styles.screenContent }>
                         <Grid container spacing={1}>
-                            <MainTitle title="Criar Conta"/>
-                            <Grid item xs={12} sm={6}>
-                                <InputText
-                                    id="name"
-                                    type="text"
-                                    label="Nome*"
-                                    value={ name }
-                                    onChange={ this.handleChange }
-                                    error={ inputErrors.name }
-                                />
-                                <InputText
-                                    id="email"
-                                    type="email"
-                                    label="Email*"
-                                    value={ email }
-                                    onChange={ this.handleChange }
-                                    error={ inputErrors.email }
-                                />
-                                <InputText
-                                    id="password"
-                                    type={ showPassword ? "text" : "password" }
-                                    label="Senha*"
-                                    value={ password }
-                                    onChange={ this.handleChange }
-                                    onClickShow={ this.handleClickShowPassword }
-                                    onMouseDown={ this.handleMouseDownPassword }
-                                    valueVisibility={ showPassword }
-                                    error={ inputErrors.password }
-                                />
-                                <InputText
-                                    id="confirmPassword"
-                                    type={ showConfirmPassword ? "text" : "password" }
-                                    label="Confirmar Senha*"
-                                    value={ confirmPassword }
-                                    onChange={ this.handleChange }
-                                    onClickShow={ this.handleClickShowConfirmPassword }
-                                    onMouseDown={ this.handleMouseDownConfirmPassword }
-                                    valueVisibility={ showConfirmPassword }
-                                    error={ inputErrors.confirmPassword }
-                                />
-                                <FileSubmit 
-                                    isFileSubmit={ isFileSubmit }
-                                    fileSubmit={ fileSubmit }
-                                    onButtonUploadClick={ this.handleClickOpenUploadDialog }
-                                    isUploadDialogOpen={ isUploadDialogOpen }
-                                    onCloseUploadDialog={ this.handleCloseUploadDialog }
-                                    onUpdateFileUploadDialog={ this.onUpdateFileUploadDialog }
-                                    onClickCancelUploadDialog={ this.handleCancelUploadDialog }
-                                    onClickConfirmUploadDialog={ this.handleConfirmUploadDialog }
-                                    onUpdateFileUploadScreen={ this.onUpdateFileUploadScreen }    
-                                />
-                                <SubmitButton 
-                                    titleButton="Criar Conta"
-                                    onClickSubmitButton={ this.onPressSubmit }
-                                />
-                            </Grid>
+                            <MainTitle title="Criar Conta" />
+                            { this.registerForm() }
                         </Grid>
                     </Typography>
                 </Grid>
@@ -267,4 +301,16 @@ const styles = {
     }
 }
 
-export default Register;
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: state.authentication.isAuthenticated,
+        requisitionError: state.authentication.requisitionError
+    }
+}
+
+export const registerContainer = connect(
+   mapStateToProps,
+   { register, restartRegister },
+)(Register)
+
+export default registerContainer;
