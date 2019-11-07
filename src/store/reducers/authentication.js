074996765b3760ef2a308b28c212d06742647b1b
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-07 00:19:53 -0300

    Refactor

--- src/store/reducers/authentication.js
+++ src/store/reducers/authentication.js
@@ -2,13 +2,28 @@
 const Authentication = (state = {}, action) => {
     switch(action.type){
         case 'RESTART_REGISTER':
-            return { ...state, requisitionError: undefined };
+            return {
+                ...state,
+                requisitionError: undefined
+            };
         case 'REGISTER_SUCCESS':
-            return { ...state, isAuthenticated: true, userData: action.payload };
+            return {
+                ...state,
+                isAuthenticated: true,
+                userData: action.payload
+            };
         case 'REGISTER_ERROR':
-            return { ...state, isAuthenticated: false, requisitionError: action.payload };
+            return {
+                ...state,
+                isAuthenticated: false,
+                requisitionError: action.payload
+            };
         case 'LOGOUT':
-            return { ...state, isAuthenticated: false, userData: undefined };
+            return {
+                ...state,
+                isAuthenticated: false,
+                userData: undefined
+            };
         default:
             return state;
     }
