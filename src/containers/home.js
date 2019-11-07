21f47e70429cf1d24666085802c659978433f4d2
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-07 00:48:24 -0300

    Add logout

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,28 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { logout } from '../store/actions';
+
+import Menu from './components/NavigationMenu/navigationMenu';
+
 
 class Home extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+
+        }
+
+        this.onPressExitButton = this.onPressExitButton.bind(this);
+    }
+
+    onPressExitButton(){
+        this.props.logout();
+    }
+
     render() {
         return (
             <div>
-
+                <Menu />
                 <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
-                <h1>Home</h1>
+                <button onClick={this.onPressExitButton}>Sair</button>
             </div>
         );
     }
 }
 
-export default Home
+function mapStateToProps(state) {
+    return {}
+}
+
+export default connect(mapStateToProps, { logout })(Home)
