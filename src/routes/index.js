0dccf596e074a86d8db5879b2ec8c1ff8bdca2f0
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-07 02:29:58 -0300

    Refactor

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import PrivateRoute from './especials/privateRoute';
 import Register from '../containers/register';
 import Login from '../containers/login';
 import PageShell from '../containers/components/PageShell/pageShell';
+import Menu from '../containers/components/NavigationMenu/navigationMenu';
 import Home from '../containers/home';
 import PersonalInfos from '../containers/personalInfos';
 import SearchMonitoring from '../containers/searchMonitoring';
@@ -13,16 +14,44 @@ import Results from '../containers/results';
 
 
 export default class App extends React.Component {
+    pathNotInBlackList(){
+        const path = window.location.pathname;
+        return path !== '/entrar' && path !== '/cadastro'
+    }
+    
     render() {
         return (
             <Router>
-                <PublicRoute exact path="/cadastro" component={ Register } />
+                {this.pathNotInBlackList() &&
+                <div>
+                    <Menu />
+                </div>
+                }
+                <PublicRoute
+                    exact path="/cadastro"
+                    component={ Register } 
+                />
                 <PublicRoute exact path="/entrar" component={ Login } />
-                <PrivateRoute exact path="/" component={ PageShell(Home) } />
-                <PrivateRoute exact path="/home" component={PageShell(Home)} />
-                <PrivateRoute exact path="/personal-infos" component={PageShell(PersonalInfos)} />
-                <PrivateRoute exact path="/search-monitoring" component={PageShell(SearchMonitoring)} />
-                <PrivateRoute exact path="/results" component={PageShell(Results)} />
+                <PrivateRoute
+                    exact path="/"
+                    component={ PageShell(Home) }
+                />
+                <PrivateRoute
+                    exact path="/home"
+                    component={ PageShell(Home) }
+                />
+                <PrivateRoute
+                    exact path="/personal-infos"
+                    component={ PageShell(PersonalInfos) }
+                />
+                <PrivateRoute
+                    exact path="/search-monitoring"
+                    component={ PageShell(SearchMonitoring) }
+                />
+                <PrivateRoute
+                    exact path="/results"
+                    component={PageShell(Results)}
+                />
             </Router>
         );
     }
