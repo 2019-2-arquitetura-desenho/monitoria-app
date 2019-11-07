a07782ebc2fb575141a18cdbec85c2b192253963
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-06 23:26:02 -0300

    Refactor

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import PrivateRoute from './especials/privateRoute';
 import Register from '../containers/register';
 import Login from '../containers/login';
+import PageShell from '../containers/components/PageShell/pageShell';
 import Home from '../containers/home';
+import PersonalInfos from '../containers/personalInfos';
+import SearchMonitoring from '../containers/searchMonitoring';
+import Results from '../containers/results';
 
 
-const App = () => (
-    <Router>
-        <Route exact path="/cadastro" component={ Register } />
-        <Route exact path="/entrar" component={ Login } />
-        <PrivateRoute exact path="/" component={ Home } />
-    </Router>
-);
-
-export default App;
+export default class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Route exact path="/cadastro" component={ Register } />
+                <Route exact path="/entrar" component={ Login } />
+                <PrivateRoute exact path="/" component={ PageShell(Home) } />
+                <PrivateRoute exact path="/home" component={PageShell(Home)} />
+                <PrivateRoute exact path="/personal-infos" component={PageShell(PersonalInfos)} />
+                <PrivateRoute exact path="/search-monitoring" component={PageShell(SearchMonitoring)} />
+                <PrivateRoute exact path="/results" component={PageShell(Results)} />
+            </Router>
+        )
+    }
+}
