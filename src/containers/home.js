182a3ac78248ddf64a7fc6b5e666348c04b7f589
Welison Almeida <welison.almeida.923@gmail.com>
2019-11-15 00:45:49 -0300

    Update homepage with new screens

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Card, CardContent, Typography, Box } from '@material-ui/core';
-import { getProfile, logout } from '../store/actions';
+import React from "react";
+import { connect } from "react-redux";
+import { getProfile, logout } from "../store/actions";
+
+import GradeDescription from "./HomePage/gradeDescription";
+import RequerimentsDescription from "./HomePage/requirementsDescription";
+import RegisterHome from "./HomePage/registerHome";
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  }
+	async componentDidMount() {
+		await this.props.getProfile(this.props.token);
+	}
 
-  async componentDidMount() {
-    await this.props.getProfile(this.props.token);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-          <p>
-            Home Wellison
-          </p>
-      </React.Fragment >
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<RegisterHome />
+				<RequerimentsDescription />
+				<GradeDescription />
+			</React.Fragment>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-  return {
-    userData: state.authentication.userData,
-    token: state.authentication.token,
-    profileData: state.userProfile.profileData
-  }
+	return {
+		userData: state.authentication.userData,
+		token: state.authentication.token,
+		profileData: state.userProfile.profileData,
+	};
 }
 
-export const homeContainer = connect(
-  mapStateToProps,
-  { getProfile, logout }
-)(Home)
+export const homeContainer = connect(mapStateToProps, { getProfile, logout })(
+	Home,
+);
 
 export default homeContainer;
