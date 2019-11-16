1ea1f4d8d474013d01f2958481be3d705b65de75
João Rodrigues <cont.joaorodrigues@gmail.com>
2019-11-16 20:25:06 -0300

    Ending Ranking Screen
    Co-authored-by: Ivan Dobbin <ivandinizdobbin2@gmail.com>

--- src/containers/results.js
+++ src/containers/results.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+
 import { 
-        MuiThemeProvider,
-        FormControl,
-        InputLabel,
-        Select,
-        MenuItem,
-        CircularProgress,
-        CssBaseline,
-        Grid,
-        Typography,
-        createMuiTheme,
-        Paper,
-        Table,    
-        TableHead,
-        TableRow,
-        TableCell,    
-        TableBody,
-    } 
-from '@material-ui/core';
+    MuiThemeProvider,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    CssBaseline,
+    Grid,
+    Typography,
+    createMuiTheme,
+    Paper,
+    Table,    
+    TableHead,
+    TableRow,
+    TableCell,    
+    TableBody,
+} from '@material-ui/core';
+
+import {
+    getProfile,
+    restartGetProfile
+} from '../store/actions';
+
+import MainError from '../components/MainError/mainError';
+
+
+const host_api = process.env.REACT_APP_URL_API;
 
 class Results extends React.Component {
 
@@ -27,90 +36,126 @@ class Results extends React.Component {
         super(props);
         
         this.state = {
-            loading: true,
-            isData: false,
+            isStudent: undefined,
 
-            materialList:[],
+            isRanked: undefined,
+            matricula: '',
+            materialList: [],
+            dataTables: [],
+
             material:'',
-            isRanked: true,
-
-            dataTables:[],
             dataChoice:[],
-            
-            matricula:'170013278'
-            
-
         }
         
         this.ranking = this.ranking.bind(this);
         this.handleChange = this.handleChange.bind(this);
-
     }
 
-    handleChange(event){
+    componentDidMount() {
+        axios.post(
+            host_api + '/get_student/',
+            {
+                token: this.props.token
+            },
+        ).then(response => {
+            const matricula = response.data.matricula;
+
+            axios.post(
+                'http://localhost:8000/home',
+                // host_api, '/get_rankings',
+                {
+                    token: this.props.token
+                }
+            ).then(response => {
+                const data = response.data.data;
+                
+                var materialList = [];
+                
+                if(data.length === 0){
+                    this.setState({
+                        isRanked: false
+                    });
+                } else {
+                    data.forEach(element => {
+                        materialList.push(element.materia);
+                    });
+                    
+                    this.setState({
+                        matricula: matricula,
+                        materialList: materialList,
+                        dataTables: data,
+                        isStudent: true,
+                        isRanked: true
+                    });
+                }
+            }).catch(error => {
+                console.log(error)
+
+                this.setState({
+                    mainError: "Erro! Verifique sua conexão com a internet e tente novamente mais tarde."
+                })
+            });
+
+        }).catch(error => {
+            console.log(error);
+
+            this.props.getProfile(this.props.token);
+            
+            if (this.props.profileData){
+                if (this.props.profileData.is_professor){
+                    this.setState({
+                        isStudent: false,
+                    });
+    
+                    // Requisição
+                } else {
+                    this.setState({
+                        mainError: "Erro! Verifique sua conexão com a internet e tente novamente mais tarde."
+                    });
+                }
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        if (this.props.requisitionError !== undefined) {
+            this.props.restartGetProfile();
+
+            this.setState({
+                mainError: "Erro! Verifique sua conexão com a internet e tente novamente mais tarde."
+            });
+        }
+    }
+
+
+    handleChange(event) {
         let dataChoice = []
         this.state.dataTables.forEach(element =>{
             if(element.materia === event.target.value){
                 dataChoice = element.ranking;
             }
         })
-        console.log(dataChoice)
+
         this.setState({
-            material:event.target.value,
-            dataChoice:dataChoice    
+            material: event.target.value,
+            dataChoice: dataChoice    
         })
     }
 
-    ranking() {
-        
-        axios.post(
-            'http://localhost:8000/home',
-            {
-            // document: document
-            }
-        ).then(response => {
-
-            //console.log(response);
-            const data = response.data.data;
-            
-            var materialList = []; 
-            
-            if(data.length === 0){
-                this.setState({
-                    isRanked:false
-                })
-            }
-            data.forEach(element => {
-                materialList.push(element.materia);
-            });
-
-            //console.log(materialList);
-            this.setState({
-                materialList:materialList,
-                isData:true,
-                dataTables:data
-            }
-            )
-
-        }).catch(error => {
-            console.log(error);
-        });
-    }
-
-    tableContent(count,nome,matr,pontu){
+    tableContent(count,nome,matr,pontu) {
         if(matr !== this.state.matricula){
-            return(
+            return (
                 <TableRow key={nome}>
                     <TableCell align="center" scope="row">
                         {count++}
                     </TableCell>
-                    <TableCell align="center" >{nome}</TableCell>
+                    <TableCell align="center">{nome}</TableCell>
                     <TableCell align="center">{matr}</TableCell>
                     <TableCell align="center">{pontu}</TableCell>
                 </TableRow>
-            )
+            );
         }
-        else{
+        else {
             return (
                 <TableRow key={nome}>
                     <TableCell align="center" scope="row">
@@ -120,90 +165,85 @@ class Results extends React.Component {
                     <TableCell align="center"><b>{matr}</b></TableCell>
                     <TableCell align="center"><b>{pontu}</b></TableCell>
                 </TableRow>
-            )
+            );
         }
     }
     
-    showData(){
-        if(!this.state.isData){
-            this.ranking();
-            return (
-               
-                    <div style={styles.progress}>
-                        <CircularProgress color="secondary" />
-                    </div>
-                
-            );
-        }
-        else if(!this.state.isRanked){
-            return <div>To com sono</div>
-        }
-        else{
-            var count = 0;
-            return(
-                <React.Fragment>
-                    <FormControl style = {{marginTop:'2%',width: "84%",marginLeft: "8%",marginRight: "8%"}}>
-                        <InputLabel id="demo-simple-select-label">Escolha a matéria</InputLabel>
-                        <Select 
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={this.state.material}
-                            onChange={this.handleChange}
-                            
-                        >   
-                            {this.state.materialList.map((element)=>{
-                                return <MenuItem value = {element}>
-                                    {element}
-                                </MenuItem>
-                            })
-                            }
-                        </Select>
-                    </FormControl>
-                    <Paper style = {{marginTop:'1%',marginBottom:'2%',width: "84%",marginLeft: "8%",marginRight: "8%"}}>
-                        <Table>
-                        <TableHead>
-                            <TableRow>
-                            <TableCell align="center">posição</TableCell>
-                            <TableCell align="center">nome</TableCell>
-                            <TableCell align="center">matrícula</TableCell>
-                            <TableCell align="center">pontuação</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {this.state.dataChoice.map(element => (
-                                count++,
-                                this.tableContent(count, element[0],element[1],element[2])
-                            ))}
-                        </TableBody>
-                        </Table>
-                    </Paper>
-                </React.Fragment>
-            )
+    showData() {
+        if (this.state.isStudent === true){
+            if (!this.state.isRanked) {
+                return (
+                    <h2 style={{color:"#141414", textAlign: "center", marginTop: "4%"}}>
+                        Ops... Você não se inscreveu em nenhuma monitoria.
+                    </h2>
+                );
+            } else {
+                var count=0;
+                return (
+                    <React.Fragment>
+                        <FormControl style = {{marginTop:'2%', width: "84%", marginLeft: "8%", marginRight: "8%"}}>
+                            <InputLabel id="demo-simple-select-label">Escolha a matéria</InputLabel>
+                            <Select 
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={this.state.material}
+                                onChange={this.handleChange}
+                                
+                            >   
+                                {this.state.materialList.map((element)=>{
+                                    return  <MenuItem value = {element}>
+                                                {element}
+                                            </MenuItem>
+                                })
+                                }
+                            </Select>
+                        </FormControl>
+                        <Paper style = {{marginTop:'1%', marginBottom:'2%', width: "84%", marginLeft: "8%", marginRight: "8%"}}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell align="center">Posição</TableCell>
+                                        <TableCell align="center">Nome</TableCell>
+                                        <TableCell align="center">Matrícula</TableCell>
+                                        <TableCell align="center">Pontuação</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {this.state.dataChoice.map(element => (
+                                        count++,
+                                        this.tableContent(count, element[0],element[1],element[2])
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Paper>
+                    </React.Fragment>
+                );
+            }
+        } else if (this.state.isStudent === false){
+            return <div>Tela professor</div>
+        } else {
+            return <div></div>
         }
     }
 
 
     render() {
-        const { classes } = this.props
-        
-        
         return (
             <MuiThemeProvider theme = {theme}>
                 <CssBaseline/>
-                <Grid container style = {{justifyContent: "center",alignItems: "center",height: "90vh"}}>
-                    <Typography component = 'div' style = {{width: "92%",marginTop: "1%",marginBottom: "1%",backgroundColor: "#ffffff",minHeight: "85%"}}>
+                <Grid container style = {{justifyContent: "center", alignItems: "center", height: "90vh"}}>
+                    <Typography component = 'div' style = {{width: "92%", marginTop: "1%", marginBottom: "1%", backgroundColor: "#ffffff", minHeight: "85%"}}>
                         <Grid container spacing = {1}>
                             <Grid item xs = {12}>
+                                <div styles={{display: "flex", justifyContent: "center", width: "100%"}}>
+                                    <MainError error={this.state.mainError} />
+                                </div>
                                 {this.showData()}
                             </Grid>
                         </Grid>
                     </Typography>
-                </Grid>
-
-
-                
+                </Grid>  
             </MuiThemeProvider>
-           
         );
     }
 }
@@ -211,7 +251,7 @@ class Results extends React.Component {
 const theme = createMuiTheme({
     palette: {
         background: {
-        default: "#42a0ed"
+        default: "#ffffff"
         },
         primary: {
         main: "#42a0ed",
@@ -223,39 +263,16 @@ const theme = createMuiTheme({
     }
 });
 
-const styles = {
-    root: {
-        backgroundColor: '#42a0ed',
-        height: '100vh',
-        paddingTop: theme.spacing(3),
-    },
-   
-    select:{
-        marginLeft: '8%',
-        marginRight: '36%',
-        height: '100%',
-        width: '100%',
-
-    },
-    progress:{
-        textAlign: "center", 
-        marginRight:"8%",
-        marginLeft:"8%",
-        marginTop:"20%",
-        marginBottom:"20%"
-       
-        
-    }
-};
-
 function mapStateToProps(state) {
     return {
       token: state.authentication.token,
+      profileData: state.userProfile.profileData
     }
 }
+
 export const resultContainer = connect(
-    mapStateToProps
-)(Results);
-  
+    mapStateToProps,
+    { restartGetProfile, getProfile }
+)(Results);  
 
 export default resultContainer;
